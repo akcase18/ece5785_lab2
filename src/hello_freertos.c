6f62d86589e326b01ff775e3bafc0bbd87215142
akcase18 <aidan.case@casefamily.us>
2024-09-11 12:51:47 -0600

    Added comments to hellow_freertos.c

--- src/hello_freertos.c
+++ src/hello_freertos.c
@@ -5,8 +5,8 @@
 #include "pico/multicore.h"
 #include "pico/cyw43_arch.h"
 
-#define MAIN_TASK_PRIORITY      ( tskIDLE_PRIORITY + 2UL )
-#define BLINK_TASK_PRIORITY     ( tskIDLE_PRIORITY + 1UL )
+#define MAIN_TASK_PRIORITY (tskIDLE_PRIORITY + 2UL)
+#define BLINK_TASK_PRIORITY (tskIDLE_PRIORITY + 1UL)
 
 #define MAIN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 #define BLINK_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
@@ -17,35 +17,42 @@ bool led_is_on;
 void blink_task(__unused void *params)
 {
     counter = 0;
-    led_is_on = false;
-    hard_assert(cyw43_arch_init() == PICO_OK);
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
-    while (true) {
-	cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_is_on);
-	if (counter++ % 47)
-	    led_is_on = !led_is_on;
-        vTaskDelay(1000);
+    led_is_on = false;                                 // Holds state of on-board LED
+    hard_assert(cyw43_arch_init() == PICO_OK);         // Makes sure cyw43 is initalized properly, otherwise fail out
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false); // Turn off on-board LED
+    while (true)
+    {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_is_on); // Set state of on-board LED to state of led_is_on
+        if (counter++ % 47)                                    // Check if counter is an even multiple of 47
+            led_is_on = !led_is_on;                            // Swtich the state of the on-board LED
+        vTaskDelay(1000);                                      // Delay 1000ms, allow FreeRTOS to schedule other things
     }
 }
 
-
+/**
+ * Switches the capitalization of any letters that are entered, and prints it out
+ */
 void main_task(__unused void *params)
 {
-    xTaskCreate(blink_task, "BlinkThread", BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
+    xTaskCreate(blink_task, "BlinkThread", BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL); // Creates and initializes the blink task
     int count = 0;
     char c;
-    while(c = getchar()) {
-	if (c <= 'z' && c >= 'a') putchar(c - 32);
-	else if (c >= 'A' && c <= 'Z') putchar(c + 32);
-	else putchar(c);
+    while (c = getchar()) // Continuous loop while characters are input into serial monitor
+    {
+        if (c <= 'z' && c >= 'a')      // Check if c is a lowercase letter
+            putchar(c - 32);           // Print character, but uppercase
+        else if (c >= 'A' && c <= 'Z') // Check if c is an uppercase letter
+            putchar(c + 32);           // Print character, but lowercase
+        else
+            putchar(c); // Print character
     }
 }
 
-int main( void )
+int main(void)
 {
-    stdio_init_all();
-    TaskHandle_t task;
-    xTaskCreate(main_task, "MainThread", MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &task);
-    vTaskStartScheduler();
+    stdio_init_all(); // Initialize all I/O
+    TaskHandle_t task; // Create a task
+    xTaskCreate(main_task, "MainThread", MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &task); // Initialize a task as the main thread
+    vTaskStartScheduler(); // Start FreeRTOS scheduler
     return 0;
 }
